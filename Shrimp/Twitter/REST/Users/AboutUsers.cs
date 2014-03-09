4959424b0111d934ed7b6fb7e23d702572d08363
UlickNormanOwen <ryoyahp@gmail.com>
2014-03-10 00:31:32 +0900

    プロフィールアップデートAPIの実装を追加

--- Shrimp/Twitter/REST/Users/AboutUsers.cs
+++ Shrimp/Twitter/REST/Users/AboutUsers.cs
@@ -91,6 +91,29 @@ namespace Shrimp.Twitter.REST.Users
         }
 
         /// <summary>
+        /// プロフィールをアップデートします
+        /// </summary>
+        /// <param name="srv"></param>
+        /// <param name="completedDelegate"></param>
+        /// <param name="errorProcess"></param>
+        /// <param name="screen_name"></param>
+        /// <param name="user_id"></param>
+        public void ProfileUpdate ( TwitterInfo srv, TwitterCompletedProcessDelegate completedDelegate, TwitterErrorProcessDelegate errorProcess, string name, string url, string location, string description )
+        {
+            List<OAuthBase.QueryParameter> q = new List<OAuthBase.QueryParameter> ();
+            if ( name != null )
+                q.Add ( new OAuthBase.QueryParameter ( "name", name ) );
+            if ( url != null )
+                q.Add ( new OAuthBase.QueryParameter ( "url", url ) );
+            if ( location != null )
+                q.Add ( new OAuthBase.QueryParameter ( "location", location ) );
+            if ( description != null )
+                q.Add ( new OAuthBase.QueryParameter ( "description", description ) );
+            this.UserShowResult = base.loadAsync ( srv, "POST", workerUserShowResult, completedDelegate, errorProcess, "account/update_profile.json", q );
+
+        }
+
+        /// <summary>
         /// 受信したデータを処理する
         /// </summary>
         /// <param name="data"></param>
