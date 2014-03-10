670c4b5f3a017ceddced01954551f92c7e545659
Tomona Nanase <nanase@users.noreply.github.com>
2014-03-10 17:53:55 +0900

    user32クラスのメンバの順序を変更

--- Shrimp/Win32API/user32.cs
+++ Shrimp/Win32API/user32.cs
@@ -5,11 +5,15 @@ namespace Shrimp.Win32API
 {
     public class user32
     {
-        [DllImport("user32.dll")]
-        static extern Int32 FlashWindowEx(ref FLASHWINFO pwfi);
-
+        private const UInt32 FLASHW_STOP = 0;        // 点滅を止める
+        private const UInt32 FLASHW_CAPTION = 1;     // タイトルバーを点滅させる
+        private const UInt32 FLASHW_TRAY = 2;        // タスクバー・ボタンを点滅させる
+        private const UInt32 FLASHW_ALL = 3;         // タスクバー・ボタンとタイトルバーを点滅させる
+        private const UInt32 FLASHW_TIMER = 4;       // FLASHW_STOPが指定されるまでずっと点滅させる
+        private const UInt32 FLASHW_TIMERNOFG = 12;  // ウィンドウが最前面に来るまでずっと点滅させる
+        
         [StructLayout(LayoutKind.Sequential)]
-        public struct FLASHWINFO
+        private struct FLASHWINFO
         {
             public UInt32 cbSize;    // FLASHWINFO構造体のサイズ
             public IntPtr hwnd;      // 点滅対象のウィンドウ・ハンドル
@@ -17,25 +21,22 @@ namespace Shrimp.Win32API
             public UInt32 uCount;    // 点滅する回数
             public UInt32 dwTimeout; // 点滅する間隔（ミリ秒単位）
         }
-
-
-        public const UInt32 FLASHW_STOP = 0;        // 点滅を止める
-        public const UInt32 FLASHW_CAPTION = 1;     // タイトルバーを点滅させる
-        public const UInt32 FLASHW_TRAY = 2;        // タスクバー・ボタンを点滅させる
-        public const UInt32 FLASHW_ALL = 3;         // タスクバー・ボタンとタイトルバーを点滅させる
-        public const UInt32 FLASHW_TIMER = 4;       // FLASHW_STOPが指定されるまでずっと点滅させる
-        public const UInt32 FLASHW_TIMERNOFG = 12;  // ウィンドウが最前面に来るまでずっと点滅させる
-
+        
         public static void FlashWindow(IntPtr hWnd, bool isStop = false)
         {
-            FLASHWINFO fInfo = new FLASHWINFO();
-            fInfo.cbSize = Convert.ToUInt32(Marshal.SizeOf(fInfo));
-            fInfo.hwnd = hWnd;
-            fInfo.dwFlags = (isStop ? FLASHW_STOP : FLASHW_ALL);
-            fInfo.uCount = 5; // 点滅回数
-            fInfo.dwTimeout = 0;
+            FLASHWINFO fInfo = new FLASHWINFO()
+            {
+                cbSize = (uint)Marshal.SizeOf(typeof(FLASHWINFO)),
+                hwnd = hWnd,
+                dwFlags = (isStop ? FLASHW_STOP : FLASHW_ALL),
+                uCount = 5, // 点滅回数
+                dwTimeout = 0,
+            };
 
             FlashWindowEx(ref fInfo);
         }
+
+        [DllImport("user32.dll")]
+        private static extern Int32 FlashWindowEx(ref FLASHWINFO pwfi);
     }
 }
