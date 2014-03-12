7de221b5201ea2619e9ded23b4b742c605213fd4
Tomona Nanase <nanase@users.noreply.github.com>
2014-03-12 19:19:06 +0900

    AdditionalEncodingクラスを追加

---
+++ Shrimp/Utils/AdditionalEncoding.cs
@@ -0,0 +1,16 @@
+﻿
+namespace System.Text
+{
+    /// <summary>
+    /// 追加のエンコードを提供します。
+    /// </summary>
+    class AdditionalEncoding
+    {
+        private static readonly Encoding shiftJIS = Encoding.GetEncoding ( 932 );
+
+        /// <summary>
+        /// Shift-JIS 形式のエンコーディングを取得します。
+        /// </summary>
+        public static Encoding ShiftJIS { get { return shiftJIS; } }
+    }
+}
