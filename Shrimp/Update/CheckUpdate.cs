2481a1561fb7e446400ffb0b60e5ad68a0c31878
UlickNormanOwen <ryoyahp@gmail.com>
2014-03-22 19:28:25 +0900

    起動時、ネットに繋がってないとここで例外が発生して落ちる問題を修正

--- Shrimp/Update/CheckUpdate.cs
+++ Shrimp/Update/CheckUpdate.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Codeplex.Data;
+using Shrimp.Log;
 
 namespace Shrimp.Update
 {
@@ -18,21 +19,8 @@ namespace Shrimp.Update
             // TODO: ファイルの取得、取得+JSONの解析用のコードに分離したほうがよさそう
             Task.Factory.StartNew(() =>
             {
-                string result;
-                var vi = FileVersionInfo.GetVersionInfo(Application.ExecutablePath);
-                var ver = vi.FileVersion.Replace(".", "");
-                var webreq = (HttpWebRequest)WebRequest.Create(UpdateUrl + ver);
-                var webres = webreq.GetResponse();
-
-                // 文字コード(EUC)を指定する
-                // FIXME: EUC?
-                var enc = Encoding.UTF8;
-
-                using (var st = webres.GetResponseStream())
-                using (var sr = new StreamReader(st, enc))
-                    // 受信して表示
-                    result = sr.ReadToEnd();
-
+				var ver = getShrimpVersion();
+				var result = checkUpdate(ver);
                 if (!string.IsNullOrWhiteSpace(result))
                 {
                     var res = DynamicJson.Parse(result);
@@ -50,5 +38,43 @@ namespace Shrimp.Update
                 }
             });
         }
+
+		/// <summary>
+		/// Shrimpのバージョンを取得
+		/// </summary>
+		/// <returns></returns>
+		public static string getShrimpVersion()
+		{
+			var vi = FileVersionInfo.GetVersionInfo(Application.ExecutablePath);
+			return vi.FileVersion.Replace(".", "");
+		}
+
+		/// <summary>
+		/// アップデート情報を取得するために、URLを叩く
+		/// </summary>
+		/// <returns>取得した結果/returns>
+		public static string checkUpdate( string shrimpVersion )
+		{
+			string result = null;
+			try
+			{
+				var webreq = (HttpWebRequest)WebRequest.Create(UpdateUrl + shrimpVersion);
+				var webres = webreq.GetResponse();
+
+				// 文字コード(EUC)を指定する
+				// FIXME: EUC?
+				var enc = Encoding.UTF8;
+
+				using (var st = webres.GetResponseStream())
+				using (var sr = new StreamReader(st, enc))
+					// 受信して表示
+					result = sr.ReadToEnd();
+			}
+			catch (Exception e)
+			{
+				LogControl.AddLogs("アップデートの取得に失敗しました。"+ e.Message +"");
+			}
+			return result;
+		}
     }
 }
